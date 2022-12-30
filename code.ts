7dddd0cb475cdefb0e2511952490ac05a04d83d0
Apoquinto <RCLlanesApo@gmail.com>
2022-12-30 14:12:00 -0600

    feat(code.ts): Add hslToRGB function

--- code.ts
+++ code.ts
@@ -32,3 +32,22 @@ interface message {
   maxElements: number,
   step: number,
 };
+
+// Method found in: https://www.30secondsofcode.org/js/s/hsl-to-rgb
+const hslToRgb = (hslValue: HSL): RGB => {
+  let rgbValue = { r: 0, g: 0, b: 0 };
+
+  hslValue.s /= 100;
+  hslValue.l /= 100;
+
+  const k = (n: number): number => (n + hslValue.h / 30) % 12;
+  const a = hslValue.s * Math.min(hslValue.l, 1 - hslValue.l);
+  const f = (n: number): number =>
+    hslValue.l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+
+  rgbValue.r = Math.round(f(0) * 255);
+  rgbValue.b = Math.round(f(4) * 255);
+  rgbValue.g = Math.round(f(8) * 255);
+
+  return rgbValue;
+}
