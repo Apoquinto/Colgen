229a2eec4528cd928e2a24c2f61c43613139f1bb
Apoquinto <RCLlanesApo@gmail.com>
2023-01-02 12:11:03 -0600

    feat(utils): remove deprecated utils

--- src/utils.ts
+++ src/utils.ts
@@ -8,10 +8,10 @@ export class Variant {
   color: HSL;
 
   constructor(pallete: string, color: HSL, position: Position) {
-    this.name = `${ pallete }-${ 1000 - color.l * 10 }`
-    this.code = "";
     this.pallete = pallete;
     this.color = color;
+    this.name = `${ pallete }-${ 1000 - color.l * 10 }`
+    this.code = `${ this.formatToHEX() } | ${ this.formatToHSL() } | ${ this.formatToRGB }`;
     this.position = position;
   }
 
@@ -32,52 +32,24 @@ export class Variant {
 
     return rgbValue;
   }
-}
 
-// Method found in: https://www.30secondsofcode.org/js/s/hsl-to-rgb
-export const hslToRgb = (source: HSL): RGB => {
-  let rgbValue = { r: 0, g: 0, b: 0 };
+  formatToRGB(): string {
+    const { r, g, b } = this.toRGB();
+    return `RGB( ${ Math.round(r * 255) } ${ Math.round(g * 255) } ${ Math.round(b * 255) } )`;
+  }
 
-  let hslValue = { ...source };
+  formatToHEX(): string {
+    const { r, g, b } = this.toRGB();
+    return `#${ numberToHex(r) }${ numberToHex(g) }${ numberToHex(b) }`.toUpperCase();
+  }
 
-  hslValue.s /= 100;
-  hslValue.l /= 100;
-
-  const k = (n: number): number => (n + hslValue.h / 30) % 12;
-  const a = hslValue.s * Math.min(hslValue.l, 1 - hslValue.l);
-  const f = (n: number): number =>
-    hslValue.l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-
-  rgbValue.r = f(0);
-  rgbValue.b = f(4);
-  rgbValue.g = f(8);
-
-  return rgbValue;
+  formatToHSL(): string {
+    const { h, s, l } = this.color;
+    return `HSL( ${ h } ${ s } ${ l } )`;
+  }
 }
 
 const numberToHex = ( value: number ) => {
   const hexRepresetation = value.toString(16);
   return hexRepresetation.length > 1 ? hexRepresetation: `0${hexRepresetation}`;
 }
-
-/**
- * Convert a RGB representation in range [0,1] to RGB representation in range [0,255]
- * @param rgb 
- */
-const getRGBDigits = ( rgb: RGB ) => ({ 
-  rDigit: Math.round(rgb.r * 255), 
-  gDigit: Math.round(rgb.g * 255),
-  bDigit: Math.round(rgb.b * 255)
-});
-
-export const getHSLCode = (hsl: HSL) => `HSL(${ hsl.h } ${ hsl.s } ${ hsl.l })`;
-
-export const getRGBCode = (rgb: RGB) => {
-  const { rDigit, gDigit, bDigit } = getRGBDigits(rgb);
-  return `RGB( ${ rDigit } ${ gDigit } ${ bDigit } )`;
-};
-
-export const getHexCode = (rgb: RGB) => {
-  const { rDigit, gDigit, bDigit } = getRGBDigits(rgb);
-  return `#${ numberToHex(rDigit) }${ numberToHex(gDigit) }${ numberToHex(bDigit) }`.toUpperCase();
-};
