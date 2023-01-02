152830ff4a01e866f01163ff849a401c9da7e792
Apoquinto <RCLlanesApo@gmail.com>
2023-01-01 21:39:30 -0600

    feat(utils): move utils to class variant

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,38 @@
-import { HSL, RGB } from "./types";
+import { HSL, Position, RGB } from "./types";
+
+export class Variant {
+  name: string;
+  code: string;
+  pallete: string;
+  position: Position;
+  color: HSL;
+
+  constructor(pallete: string, color: HSL, position: Position) {
+    this.name = `${ pallete }-${ 1000 - color.l * 10 }`
+    this.code = "";
+    this.pallete = pallete;
+    this.color = color;
+    this.position = position;
+  }
+
+  // Algorithm found in: https://www.30secondsofcode.org/js/s/hsl-to-rgb
+  toRGB(): RGB {
+    let rgbValue = { r: 0, g: 0, b: 0 };
+
+    const saturation = this.color.s / 100;
+    const lightness = this.color.l / 100;
+  
+    const k = (n: number): number => (n + this.color.h / 30) % 12;
+    const a = saturation * Math.min(lightness, 1 - lightness);
+    const f = (n: number): number => lightness - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  
+    rgbValue.r = f(0);
+    rgbValue.b = f(4);
+    rgbValue.g = f(8);
+
+    return rgbValue;
+  }
+}
 
 // Method found in: https://www.30secondsofcode.org/js/s/hsl-to-rgb
 export const hslToRgb = (source: HSL): RGB => {
